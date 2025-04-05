320fae02d5cca0953422e1d88caf138814a89f7e
anuj2110 <atrehan789@gmail.com>
2025-04-05 17:27:18 +0530

    read headers and write to response

--- app/server.go
+++ app/server.go
@@ -30,39 +30,62 @@ func main() {
 			os.Exit(1)
 		}
 		log.Println("Accepted connection from: ", conn.RemoteAddr())
-		buf,err := ReadFromRequest(conn)
-		if err!=nil{
+		buf, err := ReadFromRequest(conn)
+		if err != nil {
 			fmt.Println("Error reading from connection: ", err.Error())
 			os.Exit(1)
 		}
 		data := strings.Split(string(buf), "\r\n")
 		log.Printf("Read data: %+#v", data)
-		if strings.Contains(data[0],"echo"){
-			ReadEchoPath(data,conn)
-		} else if strings.Contains(data[0]," / "){
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		}
+		PathMapper(data, conn)
 		conn.Close()
 	}
 }
 
-func ReadEchoPath(data []string,conn net.Conn){
-	routeData := strings.Split(data[0], " ")
-	echoPath := strings.Split(routeData[1],"/")
-	last := len(echoPath)-1
-	log.Printf("Echo path: %s", echoPath[last])
-	conn.Write(fmt.Appendf(nil, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",len(echoPath[last]),echoPath[last]))
+func PathMapper(data []string, conn net.Conn) {
+	switch {
+	case strings.Contains(data[0], "echo"):
+		ReadEchoPath(data, conn)
+	case strings.Contains(data[0], " / "):
+		WriteResponse(conn, 200, "OK", "text/plain", -1, "")
+	case strings.Contains(data[0], "user-agent"):
+		ReadHeadersAndWriteResponse(data, conn)
+	default:
+		WriteResponse(conn, 404, "Not Found", "text/plain", -1, "")
+	}
 }
 
-func ReadFromRequest(conn net.Conn) ([]byte,error){
-	var buf []byte = make([]byte,1024)
-	n,err := conn.Read(buf[0:])
-	if err!=nil{
-		return nil,err
+func WriteResponse(conn net.Conn, statusCode int, statusString string, contentType string, contentLength int, body string) {
+	log.Printf("Writing response: %d %s %s %d %s", statusCode, statusString, contentType, contentLength, body)
+	if contentLength == -1 || contentType == "" {
+		conn.Write(fmt.Appendf(nil, "HTTP/1.1 %d %s\r\n\r\n", statusCode, statusString))
+		return
+	}
+	conn.Write(fmt.Appendf(nil, "HTTP/1.1 %d %s\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", statusCode, statusString, contentType, contentLength, body))
+
+}
+
+func ReadHeadersAndWriteResponse(data []string, conn net.Conn) {
+	headerData := strings.Split(data[2],": ")
+	log.Printf("Header data: %+#v",headerData)
+	WriteResponse(conn, 200, "OK", "text/plain", len(headerData[1]), headerData[1])
+}
+
+func ReadEchoPath(data []string, conn net.Conn) {
+	routeData := strings.Split(data[0], " ")
+	echoPath := strings.Split(routeData[1], "/")
+	last := len(echoPath) - 1
+	log.Printf("Echo path: %s", echoPath[last])
+	WriteResponse(conn, 200, "OK", "text/plain", len(echoPath[last]), echoPath[last])
+}
+
+func ReadFromRequest(conn net.Conn) ([]byte, error) {
+	var buf []byte = make([]byte, 1024)
+	n, err := conn.Read(buf[0:])
+	if err != nil {
+		return nil, err
 	}
 	log.Printf("Length: %d\n", n)
 	log.Println("Data: ", string(buf[0:n]))
-	return buf[0:n],nil
+	return buf[0:n], nil
 }
