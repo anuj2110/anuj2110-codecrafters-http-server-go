2d5201269db8d86cd46f704fab72d53e619a0f9c
anuj2110 <atrehan789@gmail.com>
2025-04-05 16:55:14 +0530

    added code for responding with body

--- app/server.go
+++ app/server.go
@@ -30,20 +30,39 @@ func main() {
 			os.Exit(1)
 		}
 		log.Println("Accepted connection from: ", conn.RemoteAddr())
-		var buf []byte = make([]byte, 1024)
-		n, err := conn.Read(buf[0:])
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
+		buf,err := ReadFromRequest(conn)
+		if err!=nil{
+			fmt.Println("Error reading from connection: ", err.Error())
 			os.Exit(1)
 		}
-		log.Printf("Data: %s\n", string(buf[0:n]))
-		log.Printf("Length: %d\n", n)
-		data := strings.Split(string(buf[0:n]), "\r\n")
-		if data[0] == "GET / HTTP/1.1" {
+		data := strings.Split(string(buf), "\r\n")
+		log.Printf("Read data: %+#v", data)
+		if strings.Contains(data[0],"echo"){
+			ReadEchoPath(data,conn)
+		} else if strings.Contains(data[0]," / "){
 			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		} else {
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		}
 		conn.Close()
 	}
+}
+
+func ReadEchoPath(data []string,conn net.Conn){
+	routeData := strings.Split(data[0], " ")
+	echoPath := strings.Split(routeData[1],"/")
+	last := len(echoPath)-1
+	log.Printf("Echo path: %s", echoPath[last])
+	conn.Write(fmt.Appendf(nil, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",len(echoPath[last]),echoPath[last]))
+}
+
+func ReadFromRequest(conn net.Conn) ([]byte,error){
+	var buf []byte = make([]byte,1024)
+	n,err := conn.Read(buf[0:])
+	if err!=nil{
+		return nil,err
+	}
+	log.Printf("Length: %d\n", n)
+	log.Println("Data: ", string(buf[0:n]))
+	return buf[0:n],nil
 }
