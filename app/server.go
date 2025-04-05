638b85abef622044670959318cf0a128753976d5
anuj2110 <atrehan789@gmail.com>
2025-04-05 14:35:16 +0530

    Added url parsing

--- app/server.go
+++ app/server.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -18,12 +21,29 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	conn, err := l.Accept()
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		log.Println("Accepted connection from: ", conn.RemoteAddr())
+		var buf []byte = make([]byte, 1024)
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		log.Printf("Data: %s\n", string(buf[0:n]))
+		log.Printf("Length: %d\n", n)
+		data := strings.Split(string(buf[0:n]), "\r\n")
+		if data[0] == "GET / HTTP/1.1" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+		conn.Close()
 	}
-	conn.Close()
 }
